6eaff7751f3d96d0376ed9d931f87bc1ab42773a
DESKTOP-5J639TU\Andy <only1reckoner@gmail.com>
2017-06-21 01:57:53 +0300

    change path's!

--- task1/src/test/java/ru/task1/TestCreateFile.java
+++ task1/src/test/java/ru/task1/TestCreateFile.java
@@ -1,9 +1,9 @@
 package ru.task1;
 
+import org.apache.xpath.operations.Bool;
 import org.junit.BeforeClass;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
+import org.testng.annotations.*;
 
 import java.io.File;
 import java.io.IOException;
@@ -14,29 +14,24 @@ import java.nio.file.Paths;
 /**
  * Created by Andy on 20.06.2017.
  */
-@Test
+
 public class TestCreateFile {
 
-    Path path;
+    static Path path;
 
     @BeforeMethod
     public void createTempDir() throws IOException {
         Path tempPath = Paths.get("C:\\Users\\Andy\\version-control\\junit");
         this.path = Files.createTempDirectory(tempPath, "tmp");
+        path.toFile().deleteOnExit();
+
         System.out.println("Created temp dir "+path.toString());
 
     }
 
-    @AfterClass(enabled = false)
+    @AfterMethod(enabled = true)
     public void deleteTempDir(){
-        try{
-
-            Files.delete(path);
-            System.out.println("deleted tmp dir "+ this.path);
-        }
-        catch (Exception e){
-            System.out.print("can't delete temp dir");
-        }
+    System.out.println("finished");
     }
 
     @Test
@@ -49,8 +44,45 @@ public class TestCreateFile {
         }
     }
 
-    @Test(enabled = false)
-    public void testOne(){
-        System.out.println("testing");
+    @Test
+    public void weirdLegalname() {
+        File f = new File(this.path.toString()+"\\~!a.@");
+        try {
+            f.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+
+    @Test
+    public void testEmpty() throws IOException {
+        File f = new File(this.path.toString()+"\\test1.txt");
+        f.createNewFile();
+        assert(f.length()==0);
+
+    }
+
+    @Test
+    public void negativeTest1() throws IOException {
+        File file = new File(this.path.toString()+"\\.");
+        assert(!file.createNewFile());
+
+    }
+    @Test
+    public void negativeTest2() {
+
+        File file = new File(this.path.toString() + "\\:?d.txt");
+        try {
+             file.createNewFile();
+             assert(false);
+        }
+        catch (Exception e){
+           assert(true);
+        }
+
+    }
+
+
+
 }
